0bb9b620bed177a6b74a178ccea94b4be9d111bc
Lisa Louison <lisa.louison@gmail.com>
2025-09-02 13:11:45 -0500

    fix(react-component): remove unused react import

--- comment-translator/webview-ui/src/components/CommentItem.tsx
+++ comment-translator/webview-ui/src/components/CommentItem.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
-
 const CommentItem = () => {
-  return (
-    <div>CommentItem</div>
-  )
-}
+  return <div>CommentItem</div>;
+};
 
-export default CommentItem
+export default CommentItem;
