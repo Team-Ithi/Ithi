74f73ea0d5c51e35e88581c89bec27e6f522a996
Lisa Louison <lisa.louison@gmail.com>
2025-09-02 16:57:24 -0500

    feat(footer): implement footer component

--- comment-translator/webview-ui/src/components/Footer.tsx
+++ comment-translator/webview-ui/src/components/Footer.tsx
@@ -1,5 +1,58 @@
-const Footer = () => {
-  return <div>Footer</div>;
+import { useState } from 'react';
+
+type FooterProps = {
+  source: string;
+  target: string;
+};
+
+const Footer: React.FC<FooterProps> = ({ source, target }) => {
+  const languageISO639: string[] = [
+    'en',
+    'fr',
+    'da',
+    'de',
+    'ht',
+    'haw',
+    'it',
+    'sw',
+    'vi',
+    'yo',
+    'zh',
+    'zu',
+  ];
+  const [language, setLanguage] = useState<string>(target);
+
+  const handleSelectChange = (value: string) => {
+    setLanguage(value);
+  };
+
+  return (
+    <div>
+      <div className='flex justify-between mb-1'>
+        <p>Source Language: {source.toUpperCase()} </p>
+        <label htmlFor='target_lang'>
+          Target Language:
+          <select
+            className='target_lang'
+            id='target_lang'
+            onChange={(e) => handleSelectChange(e.target.value)}
+            value={language}
+            required
+          >
+            {languageISO639.map((lang, index) => (
+              <option key={index} value={lang} disabled={lang === source}>
+                {lang.toUpperCase()}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+      <button className='mt-1 mb-1'>Translate</button>
+      <p>
+        <a className='text-red-500'>Read the docs</a> to learn more about Ithi.
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
