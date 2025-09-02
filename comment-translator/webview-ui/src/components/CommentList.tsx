e6a661409fb5ce97c06748a0ab7a19aab0253fbb
Lisa Louison <lisa.louison@gmail.com>
2025-09-02 16:58:19 -0500

    feat(commentList): implement commentList component

--- comment-translator/webview-ui/src/components/CommentList.tsx
+++ comment-translator/webview-ui/src/components/CommentList.tsx
@@ -1,5 +1,11 @@
 const CommentList = () => {
-  return <div>CommentList</div>;
+  return (
+    <div className='flex-grow flex flex-col justify-center'>
+      <p>
+        No translations yet! Click <a>'Translate'</a> to get started.
+      </p>
+    </div>
+  );
 };
 
 export default CommentList;
