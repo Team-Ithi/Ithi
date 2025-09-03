5fe1d444dc83f6befec0586263dbbac8590d5e7a
yuan-cloud <eucapop@gmail.com>
2025-09-02 19:53:56 -0500

    feat(webview-ui-component):added commentList and commentItem

--- comment-translator/webview-ui/src/components/CommentList.tsx
+++ comment-translator/webview-ui/src/components/CommentList.tsx
@@ -1,5 +1,52 @@
+import { useState} from "react";
+import CommentItem from "./CommentItem";
+import type { Row } from "./CommentItem";
+
 const CommentList = () => {
-  return <div>CommentList</div>;
+  //we start with empty container called rows and later fill them with 
+  // cards (original+translation) 
+  const [rows] = useState<Row[]>([]);
+  //we keep track of which card is open, rn nothing is open
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+
+  // useEffect(() => {
+  //   //page loads, if we get messages with data as commentData we fill rows with cards
+  //   const handler = (event: MessageEvent<unknown>) => {
+  //     const msg = event.data as { type?: string; value?: { commentData?: Row[] } };
+  //     if (msg?.type === "translationData" && Array.isArray(msg.value?.commentData)) {
+  //     setRows(msg.value.commentData);
+  //     }
+  //   };
+  //   window.addEventListener("message", handler);
+  //   //upon leaving container page stop listening 
+  //   return () => window.removeEventListener("message", handler);
+  // }, []);
+
+  const toggleIndex = (index: number) => {
+    //after clicking on a card, if open close it, if close open it
+    setSelectedIndex((prev) => (prev === index ? null : index));
+  };
+
+  if (rows.length === 0) return null; //if empty don't show anything
+
+
+  return (
+    //draw all the cards in container
+    <div>
+      {rows.map((row, index) => (
+        //for each card, draw one commentItem
+        <div key={`${row.startLine}-${row.endLine}-${index}`}
+            onClick={() => setSelectedIndex(prev => (prev === index ? null : index))}>
+          <CommentItem
+            row={row}
+            selected={index === selectedIndex}
+            onToggle={() => toggleIndex(index)}
+            //we give data to each child card, tell it should look open, give toggle option
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CommentList;
