de8872c4c3ebd0f0f2455f813810009026316bef
Lisa Louison <lisa.louison@gmail.com>
2025-09-08 17:09:44 -0500

    refactor(bing-translate): remove unused array or objects

--- comment-translator/src/mockTranslateTest.ts
+++ comment-translator/src/mockTranslateTest.ts
@@ -3,20 +3,7 @@ export const arrOfStr = [
   '你好世界 <60725>, 你好世界 <11803>.',
   '你好世界 <82914>, 你好世界 <43007>.',
 ];
-export const arrOfObj = [
-  {
-    line: 'Line One',
-    text: '你好世界 <45219>, 你好世界 <99384>.',
-  },
-  {
-    line: 'Line Two',
-    text: '你好世界 <60725>, 你好世界 <11803>.',
-  },
-  {
-    line: 'Line Three',
-    text: '你好世界 <82914>, 你好世界 <43007>.',
-  },
-];
+
 export const mockCommentData = [
   {
     startLine: '1',
