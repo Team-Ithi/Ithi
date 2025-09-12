1e19b076359943d9ceefcfa3dab41f48df18a08e
Lisa Louison <lisa.louison@gmail.com>
2025-09-12 10:19:02 -0500

    refactor(comment): remove unused code

--- comment-translator/webview-ui/src/components/CommentItem.tsx
+++ comment-translator/webview-ui/src/components/CommentItem.tsx
@@ -5,35 +5,14 @@ export type commentInfo = {
   endLine: string;
 };
 type Props = {
-  // row: Row;
-  // selected: boolean;
-  // onToggle: () => void;
   commentInfo: commentInfo;
   expanded: boolean;
 };
 const CommentItem = ({ commentInfo, expanded }: Props) => {
   const shortString =
     commentInfo.original.split(' ').slice(0, 5).join(' ') + ' ...';
-  //create item, it receives data row, selected and onToggle
-  // return (
-  //   //show icon like open close first, then show original and line range
-  //   <div onClick={onToggle}>
-  //     {selected ? '[v]' : '[>]'} {commentInfo.original} (Lines{' '}
-  //     {commentInfo.startLine}-{commentInfo.endLine})
-  //     {selected && (
-  //       //if open, show detail, if closed, do not render
-  //       <div>
-  //         <div>Original:</div>
-  //         <pre>{commentInfo.original}</pre>
-  //         <div>Translation:</div>
-  //         <pre>{commentInfo.translation}</pre>
-  //       </div>
-  //     )}
-  //   </div>
-  // );
 
   return (
-    //show icon like open close first, then show original and line range
     <div className='mb-3'>
       <div className='commentBckgrd flex justify-between px-4 py-1'>
         <code className={`${expanded ? 'visible' : 'invisible'} codePreview`}>
